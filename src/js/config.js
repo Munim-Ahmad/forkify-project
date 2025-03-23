ad83764bfad671208212ad4d0a579aac370d89f5
Munim-Ahmad <munimahmad2@gmail.com>
2025-03-23 17:52:08 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// https://forkify-api.jonas.io/
+
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b95a49ec-7915-44a3-9e2d-335fe1aade7a';
+export const MODEL_CLOSE_SEC = 2.5;
